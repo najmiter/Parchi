532c1b1cf305ea04da64ff3d61e392f4db6d643a
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-21 23:15:16 +0500

    ready to launch

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,1 +1,19 @@
+const url = new URL(window.location);
+const search = new URLSearchParams(url.search);
+
+if (search.get("Parchi")) {
+    const args = search.get("Parchi");
+    localStorage.setItem("Parchi", undefined);
+    Parchi.length = 0;
+
+    const items = args.split("_");
+    for (const item of items) {
+        const [n, q, b] = item.split("-");
+
+        Parchi.push(ParchiItem(n, q, b === "0" ? false : true));
+    }
+
+    write_localStorage();
+}
+
 revalidate_rows();
---
+++ scripts/share_btn.js
@@ -0,0 +1,22 @@
+const share_btn = document.getElementById("share-btn");
+
+share_btn.addEventListener("click", () => {
+    if (Parchi.length) {
+        const url = new URL(window.location);
+        const search = new URLSearchParams(url.search);
+
+        if (search.get("Parchi")) search.delete("Parchi");
+
+        const items = [];
+
+        Parchi.forEach(({ name, quantity, been_bought }) => {
+            const item = `${name.replaceAll(" ", "%s%")}-${quantity}-${Number(been_bought)}`;
+            items.push(item);
+        });
+        search.append("Parchi", items.join("_"));
+        url.search = search.toString();
+        history.pushState({}, "", url.toString());
+
+        navigator.clipboard.writeText(window.location.href);
+    }
+});
