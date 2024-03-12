92fe1def2c11d2e4ee7ae3ec360ad8108ae4f56a
Najam <85332859+najmiter@users.noreply.github.com>
2024-03-12 16:31:15 +0500

    styles and `reset` fixes

--- scripts/reset_item.js
+++ scripts/reset_item.js
@@ -3,4 +3,5 @@ const reset_item = document.getElementById("reset-item");
 reset_item.addEventListener("click", function () {
     menu.style.display = "none";
     mark_item_unbought(clicked_item);
+    full_items_opacity();
 });
